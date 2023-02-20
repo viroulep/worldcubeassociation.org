039aab39c72f09a54f9b4f20d6d2203e30aeefb0
Gregor Billing <gbilling@worldcubeassociation.org>
2023-02-20 12:56:42 +0100

    Add results validator panel in WRT admin dashboard
    Add option to run validations on all competitions
    
    Add back legacy checking link
    
    Fix validator competition picking form labels
    
    Remember user selection from competition chooser
    
    User feedback round 1

---
+++ WcaOnRails/app/models/result_validation_form.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class ResultValidationForm
+  ALL_VALIDATOR_NAMES = ResultsValidators::Utils::ALL_VALIDATORS.map(&:class_name)
+  VALIDATOR_WITH_FIX_NAMES = ResultsValidators::Utils::VALIDATORS_WITH_FIX.map(&:class_name)
+
+  COMP_VALIDATION_ALL = "Execute for ALL competitions"
+  COMP_VALIDATION_MANUAL = "Pick competition(s) manually"
+
+  COMP_VALIDATION_MODES = [COMP_VALIDATION_MANUAL, COMP_VALIDATION_ALL].freeze
+
+  include ActiveModel::Model
+
+  attr_accessor :validator_classes, :competition_ids
+  attr_writer :apply_fixes, :competition_selection
+
+  def competitions
+    if @competition_selection.to_sym == COMP_VALIDATION_ALL
+      Competition.ids
+    else
+      @competition_ids.split(",").uniq.compact
+    end
+  end
+
+  def competition_selection
+    @competition_selection || COMP_VALIDATION_MANUAL
+  end
+
+  def validators
+    @validator_classes.split(",").map { |v| ResultsValidators::Utils.validator_class_from_name(v) }.compact
+  end
+
+  def apply_fixes
+    ActiveModel::Type::Boolean.new.cast(@apply_fixes)
+  end
+
+  def build_validator
+    ResultsValidators::CompetitionsResultsValidator.new(
+      check_real_results: true,
+      validators: validators,
+      apply_fixes: apply_fixes,
+    )
+  end
+
+  def build_and_run
+    build_validator.tap { |v| v.validate competitions }
+  end
+end
